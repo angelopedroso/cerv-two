30fffac411ef95df287417d9a25d517897c5e54c
Angelo <solonasusonly0@gmail.com>
2026-02-22 16:39:32 -0300

    feat: add DI

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,11 +1,26 @@
 import 'package:cerv_two/core/database/database_service.dart';
 import 'package:cerv_two/core/database/i_database_service.dart';
+import 'package:cerv_two/features/product/repositories/i_product_repository.dart';
+import 'package:cerv_two/features/product/repositories/product_repository.dart';
+import 'package:cerv_two/features/product/services/i_product_service.dart';
+import 'package:cerv_two/features/product/services/product_service.dart';
 import 'package:get_it/get_it.dart';
 
-class DependencyInjection {
-  static final GetIt _sl = GetIt.instance;
+final GetIt sl = GetIt.instance;
 
+class DependencyInjection {
   static void addDI() {
-    _sl.registerSingleton<IDatabaseService>(const DatabaseService());
+    sl.registerSingleton<IDatabaseService>(const DatabaseService());
+
+    _addService();
+    _addRepository();
+  }
+
+  static void _addRepository() {
+    sl.registerLazySingleton<IProductRepository>(() => ProductRepository());
+  }
+
+  static void _addService() {
+    sl.registerFactory<IProductService>(() => ProductService());
   }
 }
