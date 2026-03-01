89fe362c1f51e21db5f0cdf27e19916247ca214a
Angelo <solonasusonly0@gmail.com>
2026-03-01 16:58:52 -0300

    refactor: add new properties to appbar & response to pop

--- lib/core/widgets/main_app_bar.dart
+++ lib/core/widgets/main_app_bar.dart
@@ -1,18 +1,22 @@
 import 'package:cerv_two/core/configs/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
+class MainAppBar<T> extends StatelessWidget implements PreferredSizeWidget {
   const MainAppBar({
     super.key,
     this.title,
     this.leading,
+    this.leadingWidth,
     this.actions,
     this.hasBackButton = false,
+    this.navigationReturn,
   });
 
   final Widget? title;
   final Widget? leading;
+  final double? leadingWidth;
   final bool hasBackButton;
+  final T? navigationReturn;
   final List<Widget>? actions;
 
   @override
@@ -21,20 +25,26 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
       decoration: const BoxDecoration(
         border: Border(bottom: BorderSide(color: AppColors.foreground)),
       ),
-      padding: const EdgeInsets.only(right: 8),
+      padding: const EdgeInsets.all(8),
       child: AppBar(
         title: title,
         actions: actions,
+        leadingWidth: leadingWidth,
+        automaticallyImplyLeading: false,
         leading:
             leading ??
             (hasBackButton
-                ? IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back_ios_new_rounded,
-                      color: AppColors.foreground,
+                ? Align(
+                    alignment: Alignment.centerLeft,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context, navigationReturn);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        color: AppColors.foreground,
+                        size: 18,
+                      ),
                     ),
                   )
                 : null),
